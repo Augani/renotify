8859b5eb63754a6837ab72739e98c6030e21618b
Augustus Otu <Augani7@gmail.com>
2020-09-21 15:07:11 +0000

    Updates

---
+++ precache-manifest.8f62061f2f68738058ab43628f917953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb8606769af7fa09dac9e0990e7b054",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcf84bdb6848dbd7ffd6",
+    "url": "./static/css/main.6576f295.chunk.css"
+  },
+  {
+    "revision": "d12c1a847b789c39df60",
+    "url": "./static/js/2.e8cfc66a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e8cfc66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf84bdb6848dbd7ffd6",
+    "url": "./static/js/main.bfe11bf7.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.bfe11bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d132f42c84d1a1e9431",
+    "url": "./static/js/runtime-main.4634db75.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c070f54b8bea2aad36aa405925dc4f5.js"
+  "./precache-manifest.8f62061f2f68738058ab43628f917953.js"
 );
 
 self.addEventListener('message', (event) => {
